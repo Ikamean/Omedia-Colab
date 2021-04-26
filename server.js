d592adf7237d555edaafcd957abd9132c5025018
Ikamean <ika.bidzishvili@gmail.com>
2021-04-26 11:11:33 +0400

    created new branch for session

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ require('dotenv').config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const session = require('express-session')
+
 const express = require('express');
 const app = express();
+
+const ONE_HOUR = 1000 * 60 * 60;
+
+
+const { 
+    PORT = 5000,
+    NODE_ENV = 'development',
+
+    SESS_LIFETIME = ONE_HOUR,
+    SESS_SECRET = "secret"
+    
+} = process.env;
+
+const IN_PROD = NODE_ENV === 'production';
 
 const Register = require('./routes/register');
 const Login = require('./routes/login');
@@ -14,12 +30,24 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+// Session \\
+app.use(session({
+    name: 'sid',
+    resave: false,
+    secret: SESS_SECRET,
+    saveUninitialized: false,
+    cookie: { 
+        maxAge: SESS_LIFETIME,
+        sameSite: true,
+        secure: IN_PROD
+    }
+}));
 
 // Routes \\
 app.use('/api/register', Register);
 app.use('/api/login', Login);
 
 app.use(express.static(`${__dirname}/views`) );
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`));
