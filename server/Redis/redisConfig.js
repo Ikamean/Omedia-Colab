25be423c38349e2b5f65c30d6b163fb8b8515e5e
ikamean <ika.bidzishvili@gmail.com>
2021-05-03 09:52:43 +0400

    #9 added try/catch block

--- server/Redis/redisConfig.js
+++ server/Redis/redisConfig.js
@@ -1,11 +1,19 @@
 const redis = require('redis');
   
-const  client    = redis.createClient({
-    port      : process.env.REDIS_PORT,               // replace with your port
-    host      : process.env.REDIS_HOST,        // replace with your hostanme or IP address
-    password  : process.env.REDIS_PASS,    // replace with your password
-  });
+try {
+    const  client    = redis.createClient({
+        port      : process.env.REDIS_PORT,               // replace with your port
+        host      : process.env.REDIS_HOST,        // replace with your hostanme or IP address
+        password  : process.env.REDIS_PASS,    // replace with your password
+      });
+
+      module.exports = { client };
+
+} catch (error) {
+    console.error(error)
+}
+
 
   //client.on('error', (err) => console.log(err));
 
-  module.exports = { client };
+ 
