dcdc6aaa3eb64830ffe16f6f622f29f07d01aac2
ikamean <ika.bidzishvili@gmail.com>
2021-05-05 08:37:58 +0400

    removed dist folder  from gitignore

---
+++ server/dist/precache-manifest.a81c7e177f361cb8d052ddcea030a76e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bb2a94c8765e60837b",
+    "url": "/css/app.cd0212ba.css"
+  },
+  {
+    "revision": "43af6e51ed8e7fab4a76",
+    "url": "/css/chunk-vendors.38f2b742.css"
+  },
+  {
+    "revision": "55a53b8c0526f8c07ccbf25a006f56c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28591a60d01fc92d302",
+    "url": "/js/about.9fdd52f4.js"
+  },
+  {
+    "revision": "34bb2a94c8765e60837b",
+    "url": "/js/app.728ea7ae.js"
+  },
+  {
+    "revision": "43af6e51ed8e7fab4a76",
+    "url": "/js/chunk-vendors.f4d108c2.js"
+  },
+  {
+    "revision": "6ed798d0605b2d323910dd4c020a7db7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
