bec3e4821d16314b400d11df1e0681416d2903be
ikamean <ika.bidzishvili@gmail.com>
2021-05-06 09:49:19 +0400

    sameSite and httpOnly

--- server/MongoDB/SessionStore/session.js
+++ server/MongoDB/SessionStore/session.js
@@ -9,7 +9,6 @@ const sess = session({
     store: store,
     cookie :{
         sameSite: 'strict',
-        secure: process.env.NODE_ENV === 'production' ? true : false,
         httpOnly: true
     }
 });
