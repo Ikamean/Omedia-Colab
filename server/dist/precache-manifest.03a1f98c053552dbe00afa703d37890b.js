f502f346a529e89853f9cafe373236653ab98348
ikamean <ika.bidzishvili@gmail.com>
2021-05-06 21:34:18 +0400

    new dist file

---
+++ server/dist/precache-manifest.03a1f98c053552dbe00afa703d37890b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b88642855e97490289a",
+    "url": "/css/app.c4973454.css"
+  },
+  {
+    "revision": "a52efa2bfa987b5df027",
+    "url": "/css/chunk-vendors.e9ed00cc.css"
+  },
+  {
+    "revision": "4ae3c63d5709aae5d7acbea1592e7a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b88642855e97490289a",
+    "url": "/js/app.fefccf3b.js"
+  },
+  {
+    "revision": "a52efa2bfa987b5df027",
+    "url": "/js/chunk-vendors.894a5507.js"
+  },
+  {
+    "revision": "6ed798d0605b2d323910dd4c020a7db7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
