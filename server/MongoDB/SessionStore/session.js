0241bb5dbd314caba3292ddd8682dfff55d0d68a
ikamean <ika.bidzishvili@gmail.com>
2021-05-06 08:57:30 +0400

    removed cookie object from session

--- server/MongoDB/SessionStore/session.js
+++ server/MongoDB/SessionStore/session.js
@@ -1,28 +1,14 @@
 const session = require('express-session')
 const store = require('./mongoStore');
 
-const ONE_HOUR = 1000 * 60 * 60;
-const { 
-    NODE_ENV = 'development',
 
-    SESS_LIFETIME = ONE_HOUR,
-    SESS_SECRET = "secret"
-    
-} = process.env;
-
-const IN_PROD = NODE_ENV === 'production';
 
 const sess = session({
     name: 'sid',
     resave: false,
-    secret: SESS_SECRET,
+    secret: 'secret',
     saveUninitialized: false,
-    store: store,
-    cookie: { 
-        maxAge: SESS_LIFETIME,
-        sameSite: 'none',
-        secure: IN_PROD
-    }
+    store: store
 });
 
 module.exports = sess;
